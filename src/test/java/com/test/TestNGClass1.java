5bb3bbc69e3d7f3b5945d26b7906be2226a02a60
Arjun <bablubaba1131227@gmail.com>
2021-08-15 20:53:17 +0530

    Committing the code again

---
+++ src/test/java/com/test/TestNGClass1.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class TestNGClass1 {
+  @Test
+  public void f() {
+	  System.out.println("Let us commit and push the code to Git repo again.");
+  }
+}
